b50dc0163ed76e5e085e6c43d7c55b4772bc19dc
Gilad elran <106150889+AntiToxics@users.noreply.github.com>
2025-10-31 14:19:07 +0200

    Update server.py

--- server.py
+++ server.py
@@ -1,36 +1,68 @@
 import socket
 import datetime
 import random
+import logging
 
 MAX_PACKET = 1024
-time = datetime.datetime.now()
 SERVER_NAME = "Gilad's Server"
 QUEUE_LEN = 1
 
-
-while True:
-    my_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    try:
-        my_socket.bind(('0.0.0.0', 1729))
-        my_socket.listen(QUEUE_LEN)
-        client_socket, client_address = my_socket.accept()
-        try :
-            request = client_socket.recv(MAX_PACKET).decode()
-            print('server received: ' + request)
+def main():
+    while True:
+        my_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        try:
+            my_socket.bind(('0.0.0.0', 1729))
+            my_socket.listen(QUEUE_LEN)
+            client_socket, client_address = my_socket.accept()
+            try :
+                request = client_socket.recv(MAX_PACKET).decode()
+                print('server received ' + request)
+            except socket.error as err:
+                print('received socket error on client socket' + str(err))
+            finally:
+                mode = client_socket.recv(MAX_PACKET).decode()
+                if(mode == "TIME"):
+                    logging.info("Client Requested Server Time")
+                    client_socket.send(f"Current date and time: {Time()}".encode())
+                elif(mode == "NAME"):
+                    logging.info("Client Requested Server Name")
+                    client_socket.send(('The Server Name Is: ' + Name()).encode())
+                elif(mode == "RAND"):
+                    logging.info("Client Requested A Random Number From 1-10")
+                    client_socket.send(('The Random Number Is: ' + RandomNum()).encode())
+                elif(mode == "EXIT"):
+                    logging.info("The Client Disconnected")
+                else:
+                    logging.warning("The Client Didn't Want Nothing")
+                    print("Warning Check server.log File")
+                client_socket.close()
         except socket.error as err:
-            print('received socket error on client socket' + str(err))
+            print('received socket error on server socket' + str(err))
         finally:
-            mode = client_socket.recv(MAX_PACKET).decode()
-            if(mode == "TIME"):
-                client_socket.send(f"The current date and time is: {time}".encode())
-            elif(mode == "NAME"):
-                client_socket.send(('The name of the server is: ' + SERVER_NAME).encode())
-            elif(mode == "RAND"):
-                client_socket.send(('The Random Number Is: ' + str(random.randint(1,10))).encode())
-            else:
-                print("Invalid argument; the client didnt choose any of the options")
-            client_socket.close()
-    except socket.error as err:
-        print('received socket error on server socket' + str(err))
-    finally:
-        my_socket.close()
+            my_socket.close()
+
+def Time():
+    time = datetime.datetime.now()
+    return time
+
+def Name():
+    return SERVER_NAME
+
+def RandomNum():
+    return str(random.randint(1,10))
+
+
+if __name__ == '__main__':
+    logging.basicConfig(
+        filename="server.log",
+        level=logging.INFO,
+        format="%(asctime)s - %(levelname)s - %(message)s",
+        filemode="a",
+    )
+
+
+    assert Name() == "Gilad's Server" , "Assert Test Failed"
+    assert 1 <= int(RandomNum()) <= 10, "Assert Test Failed"
+    logging.info("All Assert Tests Passed")
+
+    main()
